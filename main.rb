9ae1072b2626b518ff6e55a986ece3069846c054
John T Cronin <jt.cronin@outlook.com>
2017-09-11 15:59:33 -0500

    all tests for converting the string are done

--- main.rb
+++ main.rb
@@ -2,3 +2,30 @@ def upcaseWord (word)
 	wordChange = word.capitalize
 	return wordChange
 end
+
+def convertNum (num)
+	numChange = num.to_f
+	return numChange
+end
+
+def comString (one, two, three, four, five, six)
+	a = ""
+	spc = " "
+	a << one
+	a << spc
+	a << two
+	a << spc
+	a << three
+	a << spc
+	a << four
+	a << spc
+	a << five
+	a << spc
+	a << six
+	return a
+end
+
+def formatString(str)
+	newStr = str.gsub(" ", ",")
+	return newStr
+end
--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -4,8 +4,35 @@ require_relative "../main.rb"
 
 describe "convertWord" do
   it "takes the word and makes only the first character uppercase" do
-  	name = upcaseWord("NaMe")
-
-    expect(name).to eq("Name")
+  	name = upcaseWord("SoMeWoRD")
+    expect(name).to eq("Someword")
   end
+end
+
+describe "convertNumber" do
+	it "takes a number and changes it to a float with zeros after the decimal" do
+		number = convertNum(52.0000)
+		expect(number).to eq(52.00)
+	end
+end
+
+describe "combineString" do
+	it "takes six seperate strings and puts them together" do
+		one = "hello"
+		two = "darkness"
+		three = "my"
+		four = "old"
+		five = "friend"
+		six = "bar"
+		combine = comString(one, two, three, four, five, six)
+		expect(combine).to eq("hello darkness my old friend bar")
+	end
+end
+
+describe "formatString" do
+	it "takes the string, and replaces spaces with commas" do
+		str = "hello darkness my old friend bar"
+		commaStr = formatString(str)
+		expect(commaStr).to eq("hello,darkness,my,old,friend,bar")
+	end
 end
